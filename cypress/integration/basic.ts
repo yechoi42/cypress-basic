0c05b9f8dfb66c490d84cd7eca31fc5853415145
Yeji Choi <yechoi@student.42seoul.kr>
2021-05-31 12:03:44 +0900

    feat. add test code

--- cypress/integration/basic.ts
+++ cypress/integration/basic.ts
@@ -1,6 +1,17 @@
-it("should perform basic google search", () => {
-  cy.visit("https://google.com");
-  cy.get('[name="q"]')
-    .type("subscribe")
-    .type("{enter}");
+describe('counter test', () => {
+  const displayClassName = '.count-display';
+  it ('click_minus', () => {
+    cy.visit('/');
+    cy.get(displayClassName).should('value', 10);
+    cy.get('.plus-button').click().get(displayClassName).should('value', 11);
+    cy.get('.plus-button').click().get(displayClassName).should('value', 12);
+    cy.get('.plus-button').click().get(displayClassName).should('value', 12);
+  })
+  it ('click_plus', () => {
+    cy.reload();
+    cy.get(displayClassName).should('value', 10);
+    cy.get('.minus-button').click().get(displayClassName).should('value', 9);
+    cy.get('.minus-button').click().get(displayClassName).should('value', 8);
+    cy.get('.minus-button').click().get(displayClassName).should('value', 8);
+  })
 });
